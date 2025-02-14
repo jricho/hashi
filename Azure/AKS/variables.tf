c673acda1f1f1157d7c156d08e09341dae48cb1f
Jamie <jamiericho@gmail.com>
2025-02-15 10:49:01 +1100

    changed variables.tf

--- Azure/AKS/variables.tf
+++ Azure/AKS/variables.tf
@@ -1,17 +1,17 @@
 variable "project_name" {
   type        = string
   description = "Globally used project name"
-  default     = "aks-in-action"
+  default     = "aks-cluster"
 }
  
 variable "region" {
   type        = string
   description = "Azure Region that will be used"
-  default     = "West US"
+  default     = "Australia East"
 }
  
 variable "cluster_version" {
   type        = string
   description = "The Kubernetes version for our clusters"
-  default     = "1.21.2"
+  default     = "1.30.9"
 }
