3f0d4c970be79c5b79116aec83de101e2b6efb9c
Jamie <jamiericho@gmail.com>
2025-02-15 17:30:23 +1100

    changed azure.tf

--- Azure/AKS/azure.tf
+++ Azure/AKS/azure.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.19.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
 }
